eb015add70774981b2907c7095eec31888d8fae9
Paul Morrison <paul.unr@gmail.com>
2022-07-27 11:34:00 -0400

    remove formatter, using null-ls

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,106 +1,68 @@
-
 return {
 
-    -- improved movement with lightspeed
-    ["ggandor/lightspeed.nvim"] = {},
+	-- improved movement with lightspeed
+	["ggandor/lightspeed.nvim"] = {},
 
-    -- just let me repeat things
-    ["tpope/vim-repeat"] = {},
+	-- just let me repeat things
+	["tpope/vim-repeat"] = {},
 
-    -- keep my windows stable, please
-    ["luukvbaal/stabilize.nvim"] = {
-        config = function()
-            require("stabilize").setup()
-        end,
-    },
+	-- keep my windows stable, please
+	["luukvbaal/stabilize.nvim"] = {
+		config = function()
+			require("stabilize").setup()
+		end,
+	},
 
-    -- surrounding in vim, as I remember it
-    ["kylechui/nvim-surround"] = {
-        config = function()
-            require("nvim-surround").setup()
-        end,
-    },
+	-- surrounding in vim, as I remember it
+	["kylechui/nvim-surround"] = {
+		config = function()
+			require("nvim-surround").setup()
+		end,
+	},
 
-    -- dashboard, please!
-    ["goolord/alpha-nvim"] = {
-        disable = false,
-        config = function()
-            require "custom.plugins.alpha-nvim"
-        end,
-    },
+	-- dashboard, please!
+	["goolord/alpha-nvim"] = {
+		disable = false,
+		config = function()
+			require("custom.plugins.alpha-nvim")
+		end,
+	},
 
-    ["neovim/nvim-lspconfig"] = {
-        config = function()
-            require "plugins.configs.lspconfig"
-            require "custom.plugins.lspconfig"
-        end,
-    },
+	["neovim/nvim-lspconfig"] = {
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.plugins.lspconfig")
+		end,
+	},
 
-    -- overrides handles config for mason
-    -- but need to ensure it's installed correctly with plugins I think
-    ["williamboman/mason.nvim"] = {},
-    ["williamboman/mason-lspconfig.nvim"] = {},
+	-- overrides handles config for mason
+	-- but need to ensure it's installed correctly with plugins I think
+	["williamboman/mason.nvim"] = {},
+	["williamboman/mason-lspconfig.nvim"] = {},
 
-    -- Formatting with null-ls
-    ["jose-elias-alvarez/null-ls.nvim"] = {
-        after = "nvim-lspconfig",
-        config = function()
-            require "custom.plugins.null-ls"
-        end,
-    },
+	-- Formatting with null-ls
+	["jose-elias-alvarez/null-ls.nvim"] = {
+		after = "nvim-lspconfig",
+		config = function()
+			require("custom.plugins.null-ls")
+		end,
+	},
 
-    -- -- configure which LSP to install with :MasonInstallAll
-    -- ["williamboman/mason.nvim"] = {
-    --     ensure_installed = {
-    --         -- lua stuff
-    --         "lua-language-server",
-    --         "stylua",
-    --
-    --         -- web dev
-    --         "css-lsp",
-    --         "html-lsp",
-    --         "json-lsp",
-    --         "eslint_d",
-    --         "prettierd",
-    --         "tailwindcss-language-server",
-    --         "vetur-vls",
-    --     },
-    --     config = function()
-    --         require("custom.plugins.mason").setup()
-    --     end,
-    -- },
-    --
-    -- ["williamboman/mason-lspconfig.nvim"] = {
-    --     config = function()
-    --         require("custom.plugins.mason").lsp()
-    --     end,
-    --     -- module = "mason-lspconfig",
-    --     -- cmd = "VimEnter"
-    -- },
+	-- overwriting default nvim-tree setup for vinegar style
+	["kyazdani42/nvim-tree.lua"] = {
+		config = function()
+			require("custom.plugins.nvimtree")
+		end,
+	},
 
+	-- enable linting/formatting with LSP
+	["folke/trouble.nvim"] = {
+		-- after = "null-ls",
+		config = function()
+			require("custom.plugins.trouble")
+		end,
+	},
 
-    -- going back to attempting to use null-ls for formatting
-    -- ["mhartington/formatter.nvim"] = {
-    --     config = function()
-    --         require "custom.plugins.formatter"
-    --     end
-    -- },
-
-    -- overwriting default nvim-tree setup for vinegar style
-    ["kyazdani42/nvim-tree.lua"] = {
-        config = function()
-            require "custom.plugins.nvimtree"
-        end
-    },
-
-    -- enable linting/formatting with LSP
-    ["folke/trouble.nvim"] = {
-        -- after = "null-ls",
-        config = function()
-            require "custom.plugins.trouble"
-        end,
-    },
-
-    -- TODO, add in headwind support?
-    -- https://github.com/steelsojka/headwind.nvim
+	-- TODO, add in headwind support?
+	-- https://github.com/steelsojka/headwind.nvim
 }
--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -2,39 +2,39 @@
 local present, null_ls = pcall(require, "null-ls")
 
 if not present then
-    return
+	return
 end
 
 local b = null_ls.builtins
 
 local sources = {
-    -- format my files
-    b.formatting.prettierd,
+	-- format my files
+	b.formatting.prettierd,
 
-    -- Lua
-    b.formatting.stylua,
+	-- Lua
+	-- b.formatting.stylua,
 
-    -- do I want gitsigns?
-    -- b.code_actions.gitsigns,
+	-- do I want gitsigns?
+	-- b.code_actions.gitsigns,
 }
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                vim.lsp.buf.formatting_sync()
-            end,
-        })
-    end
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+				vim.lsp.buf.formatting_sync()
+			end,
+		})
+	end
 end
 
 null_ls.setup({
-    debug = true,
-    sources = sources,
-    on_attach = on_attach,
+	debug = true,
+	sources = sources,
+	on_attach = on_attach,
 })
